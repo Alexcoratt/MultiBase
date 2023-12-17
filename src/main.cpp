2ff19f2f13499ee34cf30dc33ee9c550a7fae5bc
Alexander Smirnov <alexsm.w@yandex.ru>
2023-12-17 03:43:49 +0300

    Moved short table to the new test folder and created auxillary tables

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,7 @@
 #include <BaseTable.hpp>
 #include <CSVTableConnection.hpp>
 
-#define FILENAME "../data/backloggd_games_short.csv"
-
-std::vector<std::map<std::string, AutoValue>> const people = {
-	{
-		{"id", 0},
-		{"first name", "Alexander"},
-		{"last name", "Smirnov"}
-	},
-	{
-		{"id", 1},
-		{"first name", "Ilya"},
-		{"last name", "Koltsov"}
-	},
-	{
-		{"id", 2},
-		{"first name", "Shakh"},
-		{"last name", "Isakov"}
-	}
-};
+#define FILENAME "../data/testdata/backloggd_games_short.csv"
 
 int main(int argc, char ** argv) {
 	if (argc > 2) {
@@ -38,44 +20,17 @@ int main(int argc, char ** argv) {
 	else
 		filename = defaultFilename;
 
-	ITable * table = new CSVTableConnection(filename);//new BaseTable({ "id", "first name", "last name" });
-
-	//for (auto row : people)
-	//	iter->insert(row);
-
-	auto const headings = table->getHeaders();
-	ITableIterator * iter = table->getIterator();
-	std::map<std::string, AutoValue> row;
-	std::map<std::string, AutoValue> cur;
-	unsigned int count = 0;
-	while (!iter->isEnd()) {
-		row = iter->get();
-		for (unsigned int i = 0; i < 2; ++i)
-			std::cout << headings[i] << ": " << row.at(headings[i]) << '\n';
-		std::cout << std::endl;
-		iter->next();
-		if (++count == 20) {
-			cur = row;
-			break;
-		}
+	CSVTableConnection csvTable(filename);
+	BaseTable baseTable(csvTable.getHeaders());
+	auto csvIter = csvTable.getIterator();
+	//auto btIter = baseTable.getIterator();
+	while(!csvIter->isEnd()) {
+		//btIter->insert(csvIter->get());
+		csvIter->next();
 	}
 
-	std::cout << "Current: " << cur.at(headings[0]) << std::endl;
-
-	iter->insert(cur);
-	std::cout << "Current: " << iter->isEnd() << std::endl;
-	iter->first();
-
-	while (!iter->isEnd()) {
-		row = iter->get();
-		for (unsigned int i = 0; i < 2; ++i)
-			std::cout << headings[i] << ": " << row.at(headings[i]) << '\n';
-		std::cout << std::endl;
-		iter->next();
-	}
-
-	delete iter;
-	delete table;
+	delete csvIter;
+	//delete btIter;
 
 	return 0;
 }
