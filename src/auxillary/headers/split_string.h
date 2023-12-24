1a067690f00697bbf4c5b59f706f1581b4659809
Alexander Smirnov <alexsm.w@yandex.ru>
2023-12-24 22:31:17 +0300

    Added split_string_multiscreened method

--- src/auxillary/headers/split_string.h
+++ src/auxillary/headers/split_string.h
@@ -100,4 +100,79 @@ inline void split_string_screened(char const * string, char const delimiter, cha
 	}
 }
 
+inline unsigned int count_char_multiscreened(char const * string, char search, char const * screen_symbols) {
+	unsigned int res = 0;
+	char screened = 0;
+	char last_screen_symb = '\0';
+	for (char const * symb = string; *symb != '\0'; ++symb) {
+		if (screened)
+			screened = last_screen_symb != *symb;
+		else {
+			if (count_char(screen_symbols, *symb)) {
+				screened = 1;
+				last_screen_symb = *symb;
+			}
+			else
+				res += *symb == search;
+		}
+	}
+	return res;
+}
+
+inline unsigned int strlento_multiscreened(char const * string, char const endsymb, char const * screen_symbols) {
+	unsigned int res = 0;
+	char screened = 0;
+	char last_screen_symb = '\0';
+	for (char const * symb = string; *symb != '\0' && (screened || *symb != endsymb); ++symb) {
+		if (screened)
+			screened = last_screen_symb != *symb;
+		else {
+			if (count_char(screen_symbols, *symb)) {
+				screened = 1;
+				last_screen_symb = *symb;
+			}
+		}
+		++res;
+	}
+
+	return res;
+}
+
+inline void split_string_multiscreened(char const * string, char const delimiter, char const * screen_symbols, unsigned int * resc, char *** resv) {
+	if (string[0] == '\0') {
+		*resc = 0;
+		*resv = NULL;
+		return;
+	}
+
+	*resc = count_char_multiscreened(string, delimiter, screen_symbols) + 1;
+	*resv = (char **)calloc(*resc, sizeof(char *));
+
+	unsigned int resv_count = 0;
+	unsigned int symb_count = 0;
+	char screened = 0;
+	char last_screen_symb = '\0';
+	char const * symb = string;
+
+	(*resv)[resv_count] = (char *)calloc(strlento_multiscreened(symb, delimiter, screen_symbols) + 1, sizeof(char));
+	while (*symb != '\0') {
+		if (screened)
+			screened = *symb != last_screen_symb;
+		else {
+			if (count_char(screen_symbols, *symb)) {
+				screened = 1;
+				last_screen_symb = *symb;
+			}
+			else if (*symb == delimiter) {
+				symb_count = 0;
+				(*resv)[++resv_count] = (char *)calloc(strlento_multiscreened(symb + 1, delimiter, screen_symbols) + 1, sizeof(char));
+			}
+		}
+
+		if (screened || *symb != delimiter)
+			(*resv)[resv_count][symb_count++] = *symb;
+		++symb;
+	}
+}
+
 #endif
